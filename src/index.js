7224daf7413716e093f9e5ca448e15bdb60ba1d5
DanielJDufour <daniel.j.dufour@gmail.com>
2018-11-20 06:08:18 +0000

    got working on IE

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 /*global customElements*/
-/* global fetch */
 /*global HTMLElement*/
 
 (function() {
@@ -9,7 +8,7 @@
     constructor() {
       // establish prototype chain
       super();
-      this.default_ajv_url = 'https://unpkg.com/ajv@6.5.5/dist/ajv.min.js'
+      this.default_ajv_url = 'https://unpkg.com/ajv@6.5.5/dist/ajv.min.js';
       this.default_json_editor_url = 'https://unpkg.com/jsoneditor@5.25.0';
       this.promises = {};
     }
@@ -20,8 +19,8 @@
 
     // fires after the element has been attached to the DOM
     connectedCallback() {
-      const uid = Math.ceil((Math.random() * 10e10).toString())
-      this.id = `schema-validator-${uid}`
+      const uid = Math.ceil((Math.random() * 10e10).toString());
+      this.id = `schema-validator-${uid}`;
       this.innerHTML = `<div>
         <div id="jsoneditor" style="box-sizing: border-box; height: 500px; padding: 15px; width: 100%;"></div>
         <div id="schema-validation-errors" style="box-sizing: border-box; padding: 15px; width: 100%;"></div>
@@ -37,25 +36,28 @@
     }
 
     getHTML() {
-
-      return `
-      ${this.getStyle()}
-      <div>
-        <textarea onChange=style="box-sizing: border-box; height: 500px; padding: 15px; resize: none; width: 100%;"></textarea>
-      </div>
-      `
+      return '<div><textarea onChange=style="box-sizing: border-box; height: 500px; padding: 15px; resize: none; width: 100%;"></textarea></div>';
     }
 
     getJSON(url) {
-      return fetch(url).then(response => response.json())
+      return new Promise(resolve => {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState === 4) {
+            resolve(JSON.parse(xhr.response));
+          }
+        }
+        xhr.open('GET', url, true);
+        xhr.send('');
+      });
     }
 
     getMetaSchema() {
-      return this.getJSON(this.metaschemaurl)
+      return this.getJSON(this.metaschemaurl);
     }
 
     getSchema() {
-      return this.getJSON(this.schemaurl)
+      return this.getJSON(this.schemaurl);
     }
 
     getAjv() {
@@ -63,13 +65,13 @@
         if (!this.ajv) {
           this.ajv = new window.Ajv({ allErrors: true, schemaId: 'id' });
         }
-        return this.ajv
+        return this.ajv;
       })
     }
 
     getJSONEditor() {
       return this.loadScript(this.json_editor_url + '/dist/jsoneditor.min.js')
-        .then(() => window.JSONEditor)
+        .then(() => window.JSONEditor);
     }
 
     loadScript(url) {
@@ -77,18 +79,11 @@
         this.promises[url] = new Promise(resolve => {
           const script = document.createElement("script");
           script.src = url;
-          script.onload = resolve
-          document.body.appendChild(script)
+          script.onload = resolve;
+          document.body.appendChild(script);
         })
       }
-      return Promise.resolve(this.promises[url])
-    }
-
-    getStyle() {
-      const { id } = this
-      return `
-      <style>
-      </style>`
+      return Promise.resolve(this.promises[url]);
     }
 
     validateInput() {
@@ -97,14 +92,14 @@
       console.log("starting validate")
       const text = this.querySelector('textarea').value;
       try {
-        parsed = JSON.parse(text)
+        parsed = JSON.parse(text);
       } catch (error) {
         console.error("Failed to parsed JSON.  It appears that the user did not enter valid JSON");
         console.error(error);
       }
       if (parsed) {
-        this.validate(parsed)
-        this.updateErrors(this.validate.errors)
+        this.validate(parsed);
+        this.updateErrors(this.validate.errors);
       } else {
         this.updateErrors([{ dataPath: "JSON File", message: "This does not appear to be a valid JSON file"}])
       }
@@ -112,18 +107,19 @@
     }
 
     update() {
-      this.ajv_url = this.getAttribute('ajv') || this.default_ajv_url
-      this.json_editor_url = this.getAttribute('jsoneditor') || this.default_json_editor_url
-      this.schemaurl = this.getAttribute('schema')
-      this.metaschemaurl = this.getAttribute('metaschema')
+      this.ajv_url = this.getAttribute('ajv') || this.default_ajv_url;
+      this.json_editor_url = this.getAttribute('jsoneditor') || this.default_json_editor_url;
+      this.schemaurl = this.getAttribute('schema');
+      this.metaschemaurl = this.getAttribute('metaschema');
       this.updated = Promise.all([
         this.getAjv(),
         this.getJSONEditor(),
         this.getMetaSchema(),
         this.getSchema()
-      ]).then(([ajv, JSONEditor, metaschema, schema]) => {
+      ]).then(values => {
+        const [ajv, JSONEditor, metaschema, schema] = values;
 
-        console.log("[ajv, JSONEditor, metaschema, schema]:", [ajv, JSONEditor, metaschema, schema])
+        console.log("[ajv, JSONEditor, metaschema, schema]:", [ajv, JSONEditor, metaschema, schema]);
         try {
           ajv.addMetaSchema(metaschema);
         } catch (error) {
@@ -134,22 +130,22 @@
           // create the editor
           var container = this.querySelector("#jsoneditor");
           var options = {
-            ajv,
+            ajv: ajv,
             mode: 'text',
             modes: ['text', 'tree']
           };
           this.editor = new JSONEditor(container, options, '');
-          this.editor.setSchema(schema)
-          this.editor.setText('')
+          this.editor.setSchema(schema);
+          this.editor.setText('');
         }
       })
     }
 
     updateErrors(errors) {
-      const errorBox = document.getElementById("schema-validation-errors")
-      console.log("err")
+      const errorBox = document.getElementById("schema-validation-errors");
+      console.log("err");
       if (Array.isArray(errors) && errors.length > 0) {
-        const numberOfProblems = errors.length
+        const numberOfProblems = errors.length;
         errorBox.innerHTML = `
           <div>Number of Problems: ${numberOfProblems}</div>
           ${errors.map(error => `<div>${error.dataPath}: ${error.message}</div>`).join('')}
